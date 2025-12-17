0be7f76b3ae6b3224d409b40a6f23f96c8715a1e
JustineDevs <TraderGOfficial@gmail.com>
2025-12-17 14:25:39 +0800

    update: modify VerifyHyperion.s.sol  in script

--- script/VerifyHyperion.s.sol
+++ script/VerifyHyperion.s.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract VerifyHyperion is Script {
     function run() external {
         // Note: forge verify-contract must be run manually with proper flags
         // This script is for reference only
-        
+
         console.log("To verify contracts, run:");
         console.log("");
         console.log("1. PolicyRegistry:");
@@ -25,7 +25,9 @@ contract VerifyHyperion is Script {
         console.log("  --verifier-url 'https://hyperion-testnet-explorer-api.metisdevops.link/api/' \\");
         console.log("  0xE754b6Ce911511A0D7b3A5d2d367dB305B7a7f24 \\");
         console.log("  contracts/intents/validators/SimpleSpendingValidator.sol:SimpleSpendingValidator \\");
-        console.log("  --constructor-args $(cast abi-encode 'constructor(address)' 0x92C73F9f972Bb0bdC8e3c5411345695F2E3710D0)");
+        console.log(
+            "  --constructor-args $(cast abi-encode 'constructor(address)' 0x92C73F9f972Bb0bdC8e3c5411345695F2E3710D0)"
+        );
         console.log("");
         console.log("3. IntentExecutor:");
         console.log("forge verify-contract \\");
@@ -34,6 +36,8 @@ contract VerifyHyperion is Script {
         console.log("  --verifier-url 'https://hyperion-testnet-explorer-api.metisdevops.link/api/' \\");
         console.log("  0xb078FaFE12dd9A9F2429b921Fe0fF5365AbA3cF7 \\");
         console.log("  contracts/intents/IntentExecutor.sol:IntentExecutor \\");
-        console.log("  --constructor-args $(cast abi-encode 'constructor(address)' 0xE754b6Ce911511A0D7b3A5d2d367dB305B7a7f24)");
+        console.log(
+            "  --constructor-args $(cast abi-encode 'constructor(address)' 0xE754b6Ce911511A0D7b3A5d2d367dB305B7a7f24)"
+        );
     }
 }
