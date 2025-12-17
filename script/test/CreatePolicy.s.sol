7b21a05d76bd5d0c4378fa2ab738fc0f3dbadeff
JustineDevs <TraderGOfficial@gmail.com>
2025-12-17 14:25:39 +0800

    update: modify CreatePolicy.s.sol  in test

--- script/test/CreatePolicy.s.sol
+++ script/test/CreatePolicy.s.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.24;
 
 import {Script, console} from "forge-std/Script.sol";
-import {PolicyRegistry} from "../contracts/intents/PolicyRegistry.sol";
-import {IntentTypes} from "../contracts/intents/IntentTypes.sol";
+import {PolicyRegistry} from "../../contracts/intents/PolicyRegistry.sol";
+import {IntentTypes} from "../../contracts/intents/IntentTypes.sol";
 import {KeyHelper} from "../helpers/KeyHelper.sol";
 
 contract CreatePolicy is Script, KeyHelper {
     function run() external {
         uint256 deployerPrivateKey = getPrivateKey();
         address registryAddress = vm.envAddress("POLICY_REGISTRY_ADDRESS");
-        
+
         vm.startBroadcast(deployerPrivateKey);
-        
+
         PolicyRegistry registry = PolicyRegistry(registryAddress);
-        
+
         // Create a test policy
         bytes32 policyId = keccak256("test-policy-1");
-        
+
         IntentTypes.Policy memory policy = IntentTypes.Policy({
             owner: vm.addr(deployerPrivateKey),
             allowedTargets: new address[](0), // Allow all targets
@@ -28,10 +28,10 @@ contract CreatePolicy is Script, KeyHelper {
             validBefore: type(uint256).max,
             allowedChains: new uint256[](0) // Allow all chains
         });
-        
+
         registry.setPolicy(policyId, policy);
         console.log("Policy created with ID:", vm.toString(policyId));
-        
+
         vm.stopBroadcast();
     }
 }
