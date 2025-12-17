1432782071e768a2a6dbced35e907aa639c04a72
JustineDevs <TraderGOfficial@gmail.com>
2025-12-17 14:25:39 +0800

    update: modify DeploymentConfig.sol  in helpers

--- script/helpers/DeploymentConfig.sol
+++ script/helpers/DeploymentConfig.sol
@@ -9,59 +9,38 @@ library DeploymentConfig {
     }
 
     function getHyperionTestnet() internal pure returns (ChainConfig memory) {
-        return ChainConfig({
-            chainId: 133717,
-            rpcUrl: "https://hyperion-testnet.metisdevops.link",
-            name: "Hyperion Testnet"
-        });
+        return
+            ChainConfig({
+                chainId: 133717, rpcUrl: "https://hyperion-testnet.metisdevops.link", name: "Hyperion Testnet"
+            });
     }
 
     function getMetisSepolia() internal pure returns (ChainConfig memory) {
-        return ChainConfig({
-            chainId: 59902,
-            rpcUrl: "https://metis-sepolia-rpc.publicnode.com",
-            name: "Metis Sepolia"
-        });
+        return ChainConfig({chainId: 59902, rpcUrl: "https://metis-sepolia-rpc.publicnode.com", name: "Metis Sepolia"});
     }
 
     function getMetisMainnet() internal pure returns (ChainConfig memory) {
-        return ChainConfig({
-            chainId: 1088,
-            rpcUrl: "https://andromeda.metis.io/?owner=1088",
-            name: "Metis Andromeda Mainnet"
-        });
+        return
+            ChainConfig({
+                chainId: 1088, rpcUrl: "https://andromeda.metis.io/?owner=1088", name: "Metis Andromeda Mainnet"
+            });
     }
 
     function getMantleTestnet() internal pure returns (ChainConfig memory) {
-        return ChainConfig({
-            chainId: 5003,
-            rpcUrl: "https://rpc.sepolia.mantle.xyz",
-            name: "Mantle Testnet"
-        });
+        return ChainConfig({chainId: 5003, rpcUrl: "https://rpc.sepolia.mantle.xyz", name: "Mantle Testnet"});
     }
 
     function getMantleMainnet() internal pure returns (ChainConfig memory) {
-        return ChainConfig({
-            chainId: 5000,
-            rpcUrl: "https://rpc.mantle.xyz",
-            name: "Mantle Mainnet"
-        });
+        return ChainConfig({chainId: 5000, rpcUrl: "https://rpc.mantle.xyz", name: "Mantle Mainnet"});
     }
 
     function getAvalancheFuji() internal pure returns (ChainConfig memory) {
-        return ChainConfig({
-            chainId: 43113,
-            rpcUrl: "https://api.avax-test.network/ext/bc/C/rpc",
-            name: "Avalanche Fuji"
-        });
+        return
+            ChainConfig({chainId: 43113, rpcUrl: "https://api.avax-test.network/ext/bc/C/rpc", name: "Avalanche Fuji"});
     }
 
     function getAvalancheMainnet() internal pure returns (ChainConfig memory) {
-        return ChainConfig({
-            chainId: 43114,
-            rpcUrl: "https://api.avax.network/ext/bc/C/rpc",
-            name: "Avalanche C-Chain"
-        });
+        return ChainConfig({chainId: 43114, rpcUrl: "https://api.avax.network/ext/bc/C/rpc", name: "Avalanche C-Chain"});
     }
 }
 
